d3591e030150d880e6c6623780c0dc6ba154e86d
Rudolf Markulin <rudolf.markulin@protonmail.com>
2016-08-27 15:33:55 +0200

    adding mobx-react

--- js/sin_submit.js
+++ js/sin_submit.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react/native';
+
+
+var sinSubmitState = observable({
+    timer: 0
+});
 
 export default class SinSubmit extends Component {
   static get defaultProps() {
