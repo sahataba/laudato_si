6ddc1d71a4fb46ca80cb5525112f3080b16f7c58
Rudolf Markulin <rudolf.markulin@protonmail.com>
2016-08-27 14:11:52 +0200

    navigation bar + sin submit form

--- index.ios.js
+++ index.ios.js
@@ -10,26 +10,47 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  Navigator
 } from 'react-native';
+import SinSubmit from './js/sin_submit.js';
 
 class LaudatoSi extends Component {
   render() {
     const crossPicture = require('./images/cross.jpg');
     return (
-      <View style={styles.container}>
-        <Image source={crossPicture} style={{width: 100, height: 100}}/>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ title: 'My Initial Scene', index: 0 }}
+        renderScene={(route, navigator) => {
+          return <View style={styles.container}>
+            <SinSubmit />
+            <Image source={crossPicture} style={{width: 100, height: 100}}/>
+            <Text style={styles.welcome}>
+              Welcome to React Native!
+            </Text>
+            <Text style={styles.instructions}>
+              To get started, edit index.ios.js
+            </Text>
+            <Text style={styles.instructions}>
+              Press Cmd+R to reload,{'\n'}
+              Cmd+D or shake for dev menu
+            </Text>
+          </View>
+        }}
+        navigationBar = {
+          <Navigator.NavigationBar
+            routeMapper={{
+              LeftButton: (route, navigator, index, navState) =>
+                { return (<Text>Cancel</Text>); },
+              RightButton: (route, navigator, index, navState) =>
+                { return (<Text>Done</Text>); },
+              Title: (route, navigator, index, navState) =>
+                { return (<Text>Awesome Nav Bar</Text>); },
+            }}
+            style={{backgroundColor: 'gray'}}
+          />
+        }
+      />
     );
   }
 }
