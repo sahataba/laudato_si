359a707550dbd13bbef85e1196ffc2ddb8be2cea
Rudolf Markulin <rudolf.markulin@protonmail.com>
2016-09-23 14:04:59 +0200

    testing list view datasource binding

--- js/sin_submit.js
+++ js/sin_submit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, ListView, Text, TouchableHighlight } from 'react-native';
 import {observable} from 'mobx';
 import {observer} from 'mobx-react/native';
 
@@ -8,6 +8,20 @@ var sinSubmitState = observable({
     timer: 0
 });
 
+let questionsByCategory = observable([
+  {
+    "category":"Water",
+    "questions":[
+      {
+        "question":"What do you do to reduce your water use?",
+        "answers":[
+          {"answer":"Don't boil more water than necessary when making tea", "weight":1, "marked":false}
+        ]
+      }
+    ]
+  }
+]);
+
 export default observer(class SinSubmit extends Component {
   static get defaultProps() {
     return {
@@ -15,13 +29,25 @@ export default observer(class SinSubmit extends Component {
     };
   }
 
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(questionsByCategory.slice())
+    };
+  }
+
   render() {
     return (
       <View>
         <Text>Hi! Current time is {sinSubmitState.timer}. On compinent {this.props.title}.</Text>
-        <TouchableHighlight onPress={() => sinSubmitState.timer = 5}>
+        <TouchableHighlight onPress={() => sinSubmitState.timer = sinSubmitState.timer + 1}>
           <Text>Increase by one.</Text>
         </TouchableHighlight>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.category}</Text>}
+        />
       </View>
     )
   }
